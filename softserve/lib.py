61c98c5606fe273703d47437c2727d7af96ff1df
Nigel Babu <nigelb@redhat.com>
2018-08-30 17:21:50 +0530

    Fix pylint errors

--- softserve/lib.py
+++ softserve/lib.py
@@ -2,19 +2,18 @@
 Shared library functions for softserve.
 '''
 
-import time
-import re
 import logging
 import socket
 from datetime import datetime
-from novaclient.exceptions import NotFound, Conflict
 from functools import wraps
+
 from flask import jsonify, g, redirect, url_for, request
-from softserve import db, github, celery, app
-from softserve.model import Vm, NodeRequest
 from libcloud.compute.types import Provider
 from libcloud.compute.providers import get_driver
-from libcloud.compute.deployment import SSHKeyDeployment, MultiStepDeployment
+from libcloud.compute.deployment import SSHKeyDeployment
+
+from softserve import db, github, celery, app
+from softserve.model import Vm, NodeRequest
 
 
 def organization_access_required(org):
@@ -31,7 +30,8 @@ def organization_access_required(org):
             for org_ in orgs:
                 if org_['login'] == org:
                     return func(*args, **kwargs)
-            return jsonify({"response": "You must be the member of gluster"
+            return jsonify({
+                "response": "You must be the member of gluster"
                             " organization on Github to serve"
                             " yourself machines"
                             " for testing"}), 401
@@ -46,9 +46,9 @@ def create_node(counts, name, node_request, pubkey):
     '''
     driver = get_driver(Provider.RACKSPACE)
     conn = driver(
-            app.config['USERNAME'],
-            app.config['API_KEY'],
-            region=app.config['AUTH_SYSTEM_REGION']
+        app.config['USERNAME'],
+        app.config['API_KEY'],
+        region=app.config['AUTH_SYSTEM_REGION']
     )
     flavor = conn.ex_get_size('performance1-2')
     image = conn.get_image('8bca010c-c027-4947-b9c9-adaae6e4f020')
@@ -56,15 +56,15 @@ def create_node(counts, name, node_request, pubkey):
     step = SSHKeyDeployment(pubkey)
     node_request = NodeRequest.query.get(node_request)
     for count in range(int(counts)):
-        vm_name = ''.join(['softserve-', name, '.', count+1]
+        vm_name = ''.join(['softserve-', name, '.', count+1])
         node = conn.deploy_node(
             name=vm_name, image=image, size=flavor, deploy=step
         )
-        for ip in node.public_ips:
+        for ip_addr in node.public_ips:
             try:
-                socket.inet_pton(socket.AF_INET, ip)
-                network = ip
-            except socket.error
+                socket.inet_pton(socket.AF_INET, ip_addr)
+                network = ip_addr
+            except socket.error:
                 continue
         machine = Vm(ip_address=network,
                      vm_name=vm_name,
@@ -78,20 +78,20 @@ def create_node(counts, name, node_request, pubkey):
 def delete_node(vm_name):
     driver = get_driver(Provider.RACKSPACE)
     conn = driver(
-            app.config['USERNAME'],
-            app.config['API_KEY'],
-            region=app.config['AUTH_SYSTEM_REGION']
+        app.config['USERNAME'],
+        app.config['API_KEY'],
+        region=app.config['AUTH_SYSTEM_REGION']
     )
-    vm = Vm.query.filter_by(vm_name=vm_name, state='ACTIVE').first()
+    machine = Vm.query.filter_by(vm_name=vm_name, state='ACTIVE').first()
     found = False
     for node in conn.list_nodes():
-        if node.name == vm.vm_name:
+        if node.name == machine.vm_name:
             node.destroy()
-            vm.state = 'DELETED'
-            vm.deleted_at = datetime.now()
-            db.session.add(vm)
+            machine.state = 'DELETED'
+            machine.deleted_at = datetime.now()
+            db.session.add(machine)
             db.session.commit()
             found = True
             break
-    if found == False:
+    if found is False:
         logging.exception('Server not found')
